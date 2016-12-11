33539b99f568d82e3ed8499c20b1f1860da20db2
Rob Rua <robertrua@gmail.com>
2016-12-11 06:25:53 -0500

    windward branch

--- 7-days-to-die/update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/root/steam/steamcmd.sh +login "anonymous" +force_install_dir /root/7-days-to-die +app_update 294420 +quit && rm -fr /root/Steam
--- minecraft/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/ash
-java -Xms$MINECRAFT_XMS -Xmx$MINECRAFT_XMX -jar /root/minecraft-server.jar -o true -d64 nogui
